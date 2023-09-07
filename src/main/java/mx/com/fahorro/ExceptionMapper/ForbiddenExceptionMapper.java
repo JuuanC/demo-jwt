0562c491da588821e3d4a36b90f74effa204a4b5
noehe <noevazquezh92@gmail.com>
2023-09-07 12:09:19 -0600

    Se agrega metodo de validar y validar roles

---
+++ src/main/java/mx/com/fahorro/ExceptionMapper/ForbiddenExceptionMapper.java
@@ -0,0 +1,32 @@
+package mx.com.fahorro.ExceptionMapper;
+
+import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class ForbiddenExceptionMapper implements ExceptionMapper<ForbiddenException> {
+    @Override
+    public Response toResponse(ForbiddenException exception) {
+        return Response.status(Response.Status.UNAUTHORIZED)
+                .entity(new ErrorDetails("Acceso denegado, no tienes los roles necesarios para realizar esta acción."))
+                .build();
+    }
+
+    public static class ErrorDetails {
+        private String message;
+
+        public ErrorDetails(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+    }
+}
--- src/main/java/mx/com/fahorro/resource/Test.java
+++ src/main/java/mx/com/fahorro/resource/Test.java
@@ -2,32 +2,69 @@ package mx.com.fahorro.resource;
 
 import io.smallrye.jwt.build.Jwt;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.InternalServerErrorException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import java.util.Arrays;
 import java.util.HashSet;
 
 @Path("")
+@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
 public class Test {
 
+    @Inject
+    JsonWebToken jwt;
 
     @GET
-    @Path("generar")
+    @Path("/generar")
     public String generar(){
-        String token =
-                Jwt.issuer("https://example.com/issuer")
+        return Jwt.issuer("https://example.com/issuer")
                         .upn("jdoe@quarkus.io")
                         .groups(new HashSet<>(Arrays.asList("User", "Admin")))
                         .claim(Claims.birthdate.name(), "2001-07-13")
                         .sign();
-        return token;
+    }
+
+
+    @GET
+    @Path("/validarRoles")
+    @RolesAllowed({ "User", "Admin" })
+    public Response validarAcceso(@Context SecurityContext ctx) {
+        return Response.ok("Acceso correcto, bienvenido " + ctx.getUserPrincipal().getName()).build();
     }
 
     @GET
-    @Path("Validar")
-    @RolesAllowed("Admin")
-    public boolean validar(){
-        return true;
+    @Path("/validar")
+    @RolesAllowed({ "User", "Admin" })
+    public String validar(@Context SecurityContext ctx) {
+        return getResponseString(ctx) + ", birthdate: " + jwt.getClaim("birthdate").toString();
+    }
+
+    private String getResponseString(SecurityContext ctx) {
+        String name;
+        if (ctx.getUserPrincipal() == null) {
+            name = "anonymous";
+        } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
+            throw new InternalServerErrorException("Principal and JsonWebToken names do not match");
+        } else {
+            name = ctx.getUserPrincipal().getName();
+        }
+        return String.format("hello + %s,"
+                        + " isHttps: %s,"
+                        + " authScheme: %s,"
+                        + " hasJWT: %s",
+                name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+    }
+
+    private boolean hasJwt() {
+        return jwt.getClaimNames() != null;
     }
 }
